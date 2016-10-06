3166e635c4d3a1fc8862f94b4b043deedb83e1e2
CJ Grady <CJ Grady>
2016-10-05 23:42:40 -0400

    Reorganization and cleanup

--- tests/test_tools/test_tileSplitter.py
+++ tests/test_tools/test_tileSplitter.py
@@ -1,3 +1,10 @@
+"""
+@summary: This module tests the tile splitter tool
+@author: CJ Grady
+"""
+
+from slr.tools.tileSplitter import splitTile, writeRaster
+
 def test_aaa():
    assert 1 == 1
    
