5804b6426358a6a1b347168ce545677bd198ff81
CJ Grady <cjgrady@gmail.com>
2016-10-07 19:27:22 -0400

    Multi tile test stub

---
+++ tests/test_multiTile/test_wqMultiTile.py
@@ -0,0 +1,11 @@
+"""
+@summary: Test the Work Queue multitile module
+@author: CJ Grady
+@version: 1.0
+@status: alpha
+"""
+from slr.multiTile.wqMultiTile import *
+
+def test_basic():
+   assert True
+   
