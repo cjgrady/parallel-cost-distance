b257eac0af1bd19283d9959922dc9148551e237f
CJ Grady <cjgrady@gmail.com>
2016-10-08 01:34:34 -0400

    Trying to change CC tools versions

--- install-cctools.sh
+++ install-cctools.sh
@@ -2,5 +2,5 @@
 set -ex
 wget http://ccl.cse.nd.edu/software/files/cctools-5.4.19-source.tar.gz
 tar -xzvf cctools-5.4.19-source.tar.gz
-cd cctools-6.0.2-source && ./configure --with-python-path=/usr --with-swig-path=/usr --with-perl-path=/usr && make && sudo make install
+cd cctools-5.4.19-source && ./configure --with-python-path=/usr --with-swig-path=/usr --with-perl-path=/usr && make && sudo make install
 
