9fefa2948f81b26f1234091d4b7ca317adfd07cb
CJ Grady <cjgrady@gmail.com>
2016-10-17 22:37:45 -0400

    Trying an older version of CC Tools
    Version 6.0.5 is not allowing me to pass multiple values for arguments

--- install-cctools.sh
+++ install-cctools.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 set -ex
-wget http://ccl.cse.nd.edu/software/files/cctools-6.0.5-source.tar.gz
-tar -xzvf cctools-6.0.5-source.tar.gz
-cd cctools-6.0.5-source && ./configure --with-python-path=/usr --with-swig-path=/usr --with-perl-path=/usr && make && sudo make install
+wget http://ccl.cse.nd.edu/software/files/cctools-5.4.19-source.tar.gz
+tar -xzvf cctools-5.4.19-source.tar.gz
+cd cctools-5.4.19-source && ./configure --with-python-path=/usr --with-swig-path=/usr --with-perl-path=/usr && make && sudo make install
 
