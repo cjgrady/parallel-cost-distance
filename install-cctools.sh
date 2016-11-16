b64ac42e1ed0f6f78863ae2c6a82f467ad1ce162
CJ Grady <cjgrady@gmail.com>
2016-11-15 20:38:35 -0500

    Update install-cctools.sh

--- install-cctools.sh
+++ install-cctools.sh
@@ -3,7 +3,7 @@ set -ex
 #wget http://ccl.cse.nd.edu/software/files/cctools-6.0.10-source.tar.gz
 #tar -xzvf cctools-6.0.10-source.tar.gz
 wget https://github.com/nhazekam/cctools/archive/release/6.0.zip
-unzip cctools-release-6.0.zip
+unzip 6.0.zip
 #cd cctools-6.0.10-source && ./configure --with-python-path=/usr --with-swig-path=/usr --with-perl-path=/usr && make && sudo make install
 cd cctools-release-6.0 && ./configure --with-python-path=/usr --with-swig-path=/usr --with-perl-path=/usr && make && sudo make install
 
